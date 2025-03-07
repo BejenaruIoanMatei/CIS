be8cda41297cb4db6c783e1a2c4cf1292a84d2bd
BejenaruIoanMatei <bejematei@gmail.com>
2025-03-07 13:52:50 +0200

    3. Dataset download and testing

---
+++ App/data/raw/script.bash
@@ -0,0 +1,9 @@
+:'
+    Pentru a folosi wget este nevoie de homebrew
+    $brew --version pentru a vedea daca este instalat
+    $brew install wget 
+'
+
+wget http://madm.dfki.de/files/sentinel/EuroSAT.zip
+unzip EuroSAT.zip
+rm EuroSAT.zip
